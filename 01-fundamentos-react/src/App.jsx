82c8d5514f38f407d05e842e8b81f647f99c2012
LucasSantosp09 <lucassantos.p09@gmail.com>
2023-12-23 22:14:42 -0300

    Iniciando criação do Header

--- 01-fundamentos-react/src/App.jsx
+++ 01-fundamentos-react/src/App.jsx
@@ -1,8 +1,11 @@
 import { Post } from "./Post";
+import { Header } from "./components/Header";
+import "./styles.css";
 
 export function App() {
   return (
     <div>
+      <Header />
       <Post author="Lucas Santos" content="teste teste teste" />
       <Post author="Diego Fernandes" content="teste2 teste2 teste2" />
     </div>
