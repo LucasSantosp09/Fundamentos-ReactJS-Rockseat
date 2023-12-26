24f493f0281bc29db043e0dd2baf43f06406ad94
LucasSantosp09 <lucassantos.p09@gmail.com>
2023-12-25 23:44:16 -0300

    Component: SideBar

--- 01-fundamentos-react/src/App.jsx
+++ 01-fundamentos-react/src/App.jsx
@@ -1,13 +1,20 @@
 import { Post } from "./Post";
 import { Header } from "./components/Header";
 import "./global.css";
+import styles from "./App.module.css";
+import { Sidebar } from "./components/SideBar";
 
 export function App() {
   return (
     <div>
       <Header />
-      <Post author="Lucas Santos" content="teste teste teste" />
-      <Post author="Diego Fernandes" content="teste2 teste2 teste2" />
+      <div className={styles.wraper}>
+        <Sidebar />
+        <main>
+          <Post author="Lucas Santos" content="teste teste teste" />
+          <Post author="Diego Fernandes" content="teste2 teste2 teste2" />
+        </main>
+      </div>
     </div>
   );
 }
