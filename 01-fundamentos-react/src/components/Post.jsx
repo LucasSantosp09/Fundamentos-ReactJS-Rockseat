1fa0decf644725659d86a728d3b126ef38d8f3a5
LucasSantosp09 <lucassantos.p09@gmail.com>
2023-12-27 20:20:55 -0300

    Estilizando Post

--- 01-fundamentos-react/src/components/Post.jsx
+++ 01-fundamentos-react/src/components/Post.jsx
@@ -11,27 +11,28 @@ export function Post() {
             alt=""
           />
           <div className={styles.author}>
-            <strong>Lucas Santos</strong>
-            <span>Web Develop</span>
+            <div className={styles.authorInfo}>
+              <strong>Lucas Santos</strong>
+              <span>Web Develop</span>
+            </div>
           </div>
         </div>
         <time title="27 de Dezembro de 2023" dateTime="2023-12-27 19:53:00">
           Publicado há 1 hora
         </time>
       </header>
-      <div className={styles.contet}>
+      <div className={styles.content}>
+        <p>Fala galeraa 👋 </p>
         <p>
-          Fala galeraa 👋 Acabei de subir mais um projeto no meu portifa. É um
-          projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto
-          é DoctorCare 🚀
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
         </p>
-
         <p>
           <a href="">👉 jane.design/doctorcare </a>
         </p>
-
         <p>
-          <a href="">#novoprojeto #nlw #rocketseat</a>{" "}
+          <a href="">#novoprojeto</a> <a href="">#nlw</a>{" "}
+          <a href="">#rocketseat</a>{" "}
         </p>
       </div>
     </article>
